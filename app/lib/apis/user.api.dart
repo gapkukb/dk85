e68b9615fc4f642c36fa377291c65173d8a91313
Jelon <Jelon@plus.local>
2025-08-28 19:35:55 +0800

    a

--- app/lib/apis/user.api.dart
+++ app/lib/apis/user.api.dart
@@ -1,3 +1,11 @@
-part of 'index.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/models/model.dart';
 
-final quickRegister = http.post('/app/quickRegister');
+import '/http/http.dart';
+
+final login = get('/login', LoginVo.fromJson);
+final refreshToken = get('/refresh', RefreshVo.fromJson);
+final queryUser = get('/user');
+// final queryUser = () {
+//   Dio(BaseOptions(baseUrl: 'http://localhost:9000')).get('/user');
+// };
