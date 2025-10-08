ea4db7034f34f5d9c526fc9a83ae398f8bda1ae5
jelon <ebbc8a92-9161-404c-b149-ac0b40a1ac87-217615454833954682_moongame@noreply.codeup.aliyun.com>
2025-10-08 23:41:11 +0800

    update

--- victory/scripts/.tmp/app/src/main/kotlin/com/example/victory/MethodCallHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.example.victory
-
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-
-interface IMethodCallHandler {
-    fun handle(call: MethodCall, result: MethodChannel.Result): Boolean
-}
