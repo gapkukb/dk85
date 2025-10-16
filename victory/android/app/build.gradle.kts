99324cb646c0afc2492f4e3f6f5edcaecb78fcae
jelon <ebbc8a92-9161-404c-b149-ac0b40a1ac87-217615454833954682_moongame@noreply.codeup.aliyun.com>
2025-10-16 22:32:33 +0800

    update

--- victory/android/app/build.gradle.kts
+++ victory/android/app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
